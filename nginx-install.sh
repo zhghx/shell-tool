3d6580346d0ac9fd2b335435d475d7c102399332
zhghx <zhenghegong@126.com>
2023-02-23 11:49:21 +0800

    123

--- nginx-install.sh
+++ nginx-install.sh
@@ -13,8 +13,17 @@ cd ~
 
 NGINX_DIR=nginx-1.22.1
 
-rm -rf ./${NGINX_DIR}
-rm -rf ./${NGINX_DIR}.tar.gz
+sudo rm -rf /usr/local/nginx
+sudo rm -rf /usr/local/nginx/sbin/nginx
+sudo rm -rf /usr/local/nginx/modules
+sudo rm -rf /usr/local/nginx/conf
+sudo rm -rf /usr/local/nginx/conf/nginx.conf
+sudo rm -rf /usr/local/nginx/logs/nginx.pid
+sudo rm -rf /usr/local/nginx/logs/error.log
+sudo rm -rf /usr/local/nginx/logs/access.log
+
+sudo rm -rf ./${NGINX_DIR}
+sudo rm -rf ./${NGINX_DIR}.tar.gz
 
 wget http://nginx.org/download/${NGINX_DIR}.tar.gz
 
@@ -23,3 +32,23 @@ sudo tar -zxvf ./${NGINX_DIR}.tar.gz
 cd ./${NGINX_DIR}
 
 sudo ./configure --with-http_ssl_module
+
+#Configuration summary
+#  + using system PCRE library
+#  + using system OpenSSL library
+#  + using system zlib library
+#
+#  nginx path prefix: "/usr/local/nginx"
+#  nginx binary file: "/usr/local/nginx/sbin/nginx"
+#  nginx modules path: "/usr/local/nginx/modules"
+#  nginx configuration prefix: "/usr/local/nginx/conf"
+#  nginx configuration file: "/usr/local/nginx/conf/nginx.conf"
+#  nginx pid file: "/usr/local/nginx/logs/nginx.pid"
+#  nginx error log file: "/usr/local/nginx/logs/error.log"
+#  nginx http access log file: "/usr/local/nginx/logs/access.log"
+#  nginx http client request body temporary files: "client_body_temp"
+#  nginx http proxy temporary files: "proxy_temp"
+#  nginx http fastcgi temporary files: "fastcgi_temp"
+#  nginx http uwsgi temporary files: "uwsgi_temp"
+#  nginx http scgi temporary files: "scgi_temp"
+
